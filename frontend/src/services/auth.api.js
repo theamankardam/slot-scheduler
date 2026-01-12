152acb08684f10799a06602ec2ecaaa5facb35e3
Aman Kardam <amankardam@gmail.com>
2026-01-12 13:57:58 +0530

    fixing path

--- frontend/src/services/auth.api.js
+++ frontend/src/services/auth.api.js
@@ -1,17 +1,17 @@
 import axiosInstance from "./axiosInstance";
 
 export const loginApi = async (data) => {
-    const res = await axiosInstance.post("/api/login", data);
+    const res = await axiosInstance.post("/api/auth/login", data);
     return res.data;
 };
 
 export const signupApi = async (data) => {
-    const res = await axiosInstance.post("/api/signup", data);
+    const res = await axiosInstance.post("/api/auth/signup", data);
     return res.data;
 };
 
 
 export const getCurrentUser = async () => {
-    const res = await axiosInstance.get("/api/currentUser");
+    const res = await axiosInstance.get("/api/auth/currentUser");
     return res.data;
 }
